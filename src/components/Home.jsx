89866920fca77ed74ce3011a8eb017257b232180
JonesTPG <joonas.ryynanen1@gmail.com>
2020-06-03 12:23:26 +0300

    home page content.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { Box, Flex, Text, Button } from '@chakra-ui/core';
+
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <>
-      <p> home page</p>
+      <Flex w="100vw" justify="center" p={10} direction="row">
+        <Text fontSize="4xl">Front page of card app</Text>
+      </Flex>
+      <Link to={'/cards'}>
+        <Box textAlign="center">
+          <Button>Show cards</Button>
+        </Box>
+      </Link>
     </>
   );
 };
