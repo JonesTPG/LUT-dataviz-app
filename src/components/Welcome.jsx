e850a26a0b876bd3bafb0f40ee620f3a30797c4f
JonesTPG <joonas.ryynanen1@gmail.com>
2020-06-29 15:21:42 +0300

    application client state testing.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Box, Flex, Text, Button } from '@chakra-ui/core';
-
 import { Link } from 'react-router-dom';
 
+import { getState } from '../utils/localStorage';
+
 const Welcome = () => {
+  let defaultState = {
+    hasDoneAssignment: false,
+    hasVisitedSurvey: false,
+    hasSentSurvey: false
+  };
+
+  useEffect(() => {
+    let applicationState;
+    getState() === null
+      ? (applicationState = defaultState)
+      : (applicationState = getState());
+    console.log('on refresh');
+    console.log('current state:');
+    console.log(applicationState);
+  });
+
   return (
     <>
       <Flex justify="center" p={10} direction="row">
@@ -13,11 +30,6 @@ const Welcome = () => {
       <Link to={'/assignment'}>
         <Box textAlign="center">
           <Button>Proceed to assignment</Button>
-        </Box>
-      </Link>
-      <Link to={'/cards'}>
-        <Box mt={15} textAlign="center">
-          <Button>Show cards (remove)</Button>
         </Box>
       </Link>
     </>
