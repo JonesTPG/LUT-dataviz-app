03fd89c41257c4f4d24f10c9a7fae487dee98649
JonesTPG <joonas.ryynanen1@gmail.com>
2020-07-09 10:02:30 +0300

    WIP: question layout.

--- src/components/questions/CardQuestion.jsx
+++ src/components/questions/CardQuestion.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Flex, Image, Text, Box } from '@chakra-ui/core';
 
 import { useApiUrl } from '../../hooks/common';
 
@@ -10,9 +11,24 @@ const CardQuestion = ({ data }) => {
   //TODO: add better UI with chakra components
   return (
     <>
-      <p>{text}</p>
-      <p>{order}</p>
-      <p>{image.url}</p>
+      <Box
+        m={50}
+        p={50}
+        textAlign="center"
+        borderColor="blue.500"
+        borderWidth={10}
+      >
+        <Flex justifyContent="space-evenly">
+          <Image
+            w={['100%', '80%', '40%', '40%']}
+            rounded="md"
+            src={API_URL + image.url}
+          />
+          <Text mt={1} fontSize={['l', 'l', 'xl', 'xl']}>
+            {text}
+          </Text>
+        </Flex>
+      </Box>
     </>
   );
 };
