35def37deb369774711b6be46d43b7f2281798ec
JonesTPG <joonas.ryynanen1@gmail.com>
2020-07-13 11:19:10 +0300

    application version randomization. removed old util functions.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 import { Box, Flex, Text, Button } from '@chakra-ui/core';
 
+import { useStickyState } from '../hooks/common';
+import { getRandomInt } from '../utils/randomize';
+
 const Welcome = ({ show, setPage, setProgress }) => {
+  /* TODO:
+      - add logic that determines the application version for the user
+      - store the application version in local storage so that it doesn't change on browser refresh
+  */
+  const [applicationVersion, setApplicationVersion] = useStickyState(
+    getRandomInt(1, 3),
+    'application-version'
+  );
+
   if (!show) {
     return null;
   }
